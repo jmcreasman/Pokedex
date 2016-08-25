bea2f0206c988497aaf7f9ee712a5ac6dd7ec496
Jon <jmcreasman89@gmail.com>
2016-08-25 11:53:29 -0400

    test pokemon working with angular

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,14 @@
-angular.module('nationalPokedex', []);
+(function() {
+  'use strict';
+  var app = angular.module('nationalPokedex', []);
+
+  app.controller('DisplayPokemon', function(){
+    this.product = testpokemon;
+  });
+
+  var testpokemon = {
+    name: "Bulbasaur",
+    number: 1,
+    image: "Coming Soon!",
+  };
+}());
--- app/js/controllers/display-pokemon-controller.js
+++ app/js/controllers/display-pokemon-controller.js
@@ -1,14 +1,17 @@
-// angular.module('pokedex')
-// .controller('DisplayPokemon', ['$http', function($http) {
-//     var pokemon = this;
-//
-//     // pokemon.bio = [];
-//
-//     $http.get('../../pokemon.json')
-//       .success(function(data) {
-//         pokemon.pokemon = data;
-//       })
-//       .error(function(data, status) {
-//         console.error('Repos error', status, data);
-//       });
-//   }]);
+(function() {
+  'use strict';
+  // angular.module('pokedex')
+  // .controller('DisplayPokemon', ['$http', function($http) {
+  //     var pokemon = this;
+  //
+  //     // pokemon.bio = [];
+  //
+  //     $http.get('../../pokemon.json')
+  //       .success(function(data) {
+  //         pokemon.pokemon = data;
+  //       })
+  //       .error(function(data, status) {
+  //         console.error('Repos error', status, data);
+  //       });
+  //   }]);
+}());
