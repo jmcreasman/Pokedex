2ac08ffebb7bc1fda61e13b78dbc92d7b4a789bf
Jon <jmcreasman89@gmail.com>
2016-08-25 09:57:12 -0400

    attempting to get angular working

--- app/js/controllers/display-pokemon-controller.js
+++ app/js/controllers/display-pokemon-controller.js
@@ -1,2 +1,14 @@
 angular.module('pokedex')
-.controller('DisplayPokemon');
+.controller('DisplayPokemon', ['$http', function($http) {
+    var pokemon = this;
+
+    // pokemon.bio = [];
+
+    $http.get('../../pokemon.json')
+      .success(function(data) {
+        pokemon.pokemon = data;
+      })
+      .error(function(data, status) {
+        console.error('Repos error', status, data);
+      });
+  }]);
