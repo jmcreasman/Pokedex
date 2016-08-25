38c19385de536a753a2beeddd9a2310e4345eb0f
Jon <jmcreasman89@gmail.com>
2016-08-25 16:37:44 -0400

    angular working with multiple pokemon

--- app/js/app.js
+++ app/js/app.js
@@ -2,13 +2,13 @@
   'use strict';
   var app = angular.module('nationalPokedex', []);
 
-  app.controller('DisplayPokemon', function(){
-    this.product = testpokemon;
+  app.controller('DisplayPokemon', function() {
+    this.products = testpokemons;
   });
 
-  var testpokemon = {
-    name: "Bulbasaur",
-    number: 1,
-    image: "Coming Soon!",
-  };
+  var testpokemons = [
+    { name: "Bulbasaur", number: 1, image: "Coming Soon!"},
+    { name: "Ivysaur", number: 2, image: "Coming Soon!"},
+    { name: "Venusaur", number: 3, image: "Coming Soon!"},
+  ];
 }());
