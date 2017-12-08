4e21b80bbc8aec99b031482c758d8ccdb0fb9f71
Jon <jmcreasman89@gmail.com>
2017-12-08 14:09:19 -0500

    html structure based on work done on insurance demo app

--- pokedex/src/components/Dashboard.vue
+++ pokedex/src/components/Dashboard.vue
@@ -1,6 +1,53 @@
 <template>
   <div class="app">
-    Hello World
+    <header>HEADER PLACEHOLDER</header>
+    <div class="main">
+      <h1>
+        Welcome back,<br> Trainer Red
+      </h1>
+      <div class="top-section message">
+        <div class="widthOfContent">
+          <img class="icon" src="">
+          <div class="column">
+            <h2>06.14.17 - Message</h2>
+            There is a payment due soon.
+          </div>
+        </div>
+        <img class="open-arrow" src="">
+      </div>
+      <div class="top-section my-policy">
+        <div class="widthOfContent">
+          <img class="icon" src="">
+          <div class="column">
+            <h2>My Policy</h2>
+            1234567-XXXX
+          </div>
+        </div>
+        <img class="open-arrow" src="">
+      </div>
+
+      <section>
+        <button class="column left">
+          <img src="">
+          <span>Make A Payment</span>
+        </button>
+        <button class="column right">
+          <img src="">
+          <span>Payment History</span>
+        </button>
+      </section>
+      <section>
+        <button class="column left">
+          <img src="">
+          <span>Accident Help</span>
+        </button>
+        <button class="column right">
+          <img src="">
+          <span>Claim History</span>
+        </button>
+      </section>
+    </div>
+    <footer class="footer">FOOTER PLACEHOLDER</footer>
   </div>
 </template>
 
@@ -18,7 +65,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   .app {
-    background: linear-gradient(-41deg, #2A354C 1%, #293951 41%, #09B9EE 100%);
+    /* background: linear-gradient(-41deg, #2A354C 1%, #293951 41%, #09B9EE 100%); */
     height: 100vh;
   }
 </style>
