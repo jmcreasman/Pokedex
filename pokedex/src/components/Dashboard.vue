2ebffce7130da69dd59dde7868cb57330c163d85
Jon <jmcreasman89@gmail.com>
2017-12-11 15:53:45 -0500

    incorporated styles from demo app to rewrite at a later date

--- pokedex/src/components/Dashboard.vue
+++ pokedex/src/components/Dashboard.vue
@@ -52,6 +52,10 @@
 </template>
 
 <script>
+
+import Vue from 'vue';
+import router from '../router';
+
 export default {
   name: 'Dashboard',
   data () {
@@ -64,8 +68,184 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
   .app {
-    /* background: linear-gradient(-41deg, #2A354C 1%, #293951 41%, #09B9EE 100%); */
+    background: linear-gradient(-41deg, #2A354C 1%, #293951 41%, #09B9EE 100%);
     height: 100vh;
   }
+
+  .main {
+    top: -60px;
+    color: white;
+    height: 75%;
+    padding: 0px 18px;
+    order: 1;
+    flex: 1;
+    z-index: 1;
+    width: 100%;
+    position: relative;
+    /* transition: .4s cubic-bezier(0.215, 0.61, 0.355, 1); */
+  }
+
+  .main-content {
+    position: absolute;
+    overflow-y: scroll;
+    overflow-x: visible;
+  }
+
+  .invisible-scrollbar::-webkit-scrollbar {
+    display: none;
+  }
+
+  html::-webkit-scrollbar, body::-webkit-scrollbar {
+    display: none;
+  }
+
+  button {
+    width: 165px;
+    height: 100px;
+    color: white;
+    background: none;
+    /* opacity: 0; */
+    position: relative;
+    /* animation: bottom-slide-in 0.3s forwards;
+    animation-delay: 2s;
+    -webkit-animation: bottom-slide-in  0.3s forwards;
+    -webkit-animation-delay: 2s; */
+  }
+
+  button.column {
+    font-size: 14px;
+    letter-spacing: 0.4px;
+    flex-direction: column;
+    width: auto;
+    border: none;
+  }
+
+  button.column.left {
+    margin-right: 5px;
+  }
+
+  button.column.right {
+    margin-left: 5px;
+  }
+
+  .column {
+    font-size: 14px;
+    letter-spacing: 0.4px;
+    flex-direction: column;
+    width: auto;
+    border: none;
+  }
+
+  div, section, button {
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+    justify-content: center;
+  }
+
+  h1 {
+    font-size: 27px;
+    font-weight: 100;
+    color: #FFFFFF;
+    letter-spacing: 0;
+    line-height: 30px;
+    width: 100%;
+    display: flex;
+    text-align: left;
+    margin-top: 5px;
+    margin-bottom: 5px;
+    /* animation: fadein 0.5s forwards;
+    -webkit-animation: fadein 0.5s forwards; */
+  }
+
+  h2 {
+    font-size: 21px;
+    font-weight: 100;
+    color: #FFFFFF;
+    letter-spacing: 0;
+    margin-top: 2px;
+    margin-bottom: 2px;
+  }
+
+  img {
+    margin-left: 16px;
+    margin-right: 16px;
+    margin-bottom: 10px;
+  }
+
+  .icon {
+    margin-left: 25px;
+    margin-right: 25px;
+    margin-bottom: 0px;
+  }
+
+  .open-arrow {
+    margin-bottom: 0px;
+  }
+
+  section.column {
+    width: 50%;
+    justify-content: center;
+  }
+
+  section .column {
+    border: 1px solid #148FBB;
+    box-shadow: 0 2px 4px 0 rgba(0,0,0,0.18);
+    border-radius: 11px;
+    width: 48%;
+    box-sizing: border-box;
+    align-items: center;
+  }
+
+  .top-section {
+    justify-content: space-between;
+    background: rgba(0,0,0,0.40);
+    border: 1px solid #148FBB;
+    box-shadow: 0 2px 4px 0 rgba(0,0,0,0.18);
+    border-radius: 11px;
+    height: 80px;
+    align-items: center;
+    position: relative;
+    /* opacity: 0; */
+  }
+
+  .message {
+    animation: left-slide-in 0.3s forwards;
+    animation-delay: 0.5s;
+    -webkit-animation: left-slide-in  0.3s forwards;
+    -webkit-animation-delay: 0.5s;
+  }
+
+  .my-policy {
+    animation: left-slide-in 0.3s forwards;
+    animation-delay: 0.7;
+    -webkit-animation: left-slide-in  0.3s forwards;
+    -webkit-animation-delay: 0.7s;
+  }
+
+  .widthOfContent {
+    width: auto;
+    align-items: center;
+  }
+
+/* For when working footer is complete */
+  /* #my-agent {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+
+    display: flex;
+    height: 90vh;
+    flex-direction: column;
+    overflow: hidden;
+  } */
 </style>
