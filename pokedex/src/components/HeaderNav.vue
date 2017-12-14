4def93940c870273c3aaeae085749f1f06583300
Jon <jmcreasman89@gmail.com>
2017-12-14 14:11:17 -0500

    setup hello world header component and added it to dashboard

---
+++ pokedex/src/components/HeaderNav.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Hello World!
+  </div>
+</template>
+
+<script>
+
+  import router from '../router';
+  import Vue from 'vue';
+
+  export default {
+  name: 'HeaderNav',
+  data () {
+  }
+}
+</script>
+
+<style scoped>
+</style>
