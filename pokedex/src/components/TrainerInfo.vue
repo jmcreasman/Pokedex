cdd8a53d11a266418ad670c892974b66b9005d7e
Jon <jmcreasman89@gmail.com>
2018-01-24 14:41:47 -0500

    very basic setup of footer component

---
+++ pokedex/src/components/TrainerInfo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Hello Footer!
+  </div>
+</template>
+
+<script>
+  import router from '../router';
+  import Vue from 'vue';
+
+  export default {
+  name: 'TrainerInfo',
+  data () {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
