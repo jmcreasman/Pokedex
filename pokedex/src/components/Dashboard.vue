f7a615c4848a4970f86f917d0c0687c831ce392a
Jon <jmcreasman89@gmail.com>
2018-02-09 14:53:55 -0500

    set routing to pokedex from homepage

--- pokedex/src/components/Dashboard.vue
+++ pokedex/src/components/Dashboard.vue
@@ -16,7 +16,7 @@
       </div>
 
       <section>
-        <button class="column left">
+        <button v-on:click="navigateTo('/pokedex')" class="column left">
           <img src="../assets/make_payment.svg">
           <span>Pokedex</span>
         </button>
@@ -52,10 +52,15 @@ import Pokedex from '../components/Pokedex.vue';
 export default {
   name: 'Dashboard',
 
-  data () {
+  data: function () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  methods: {
+    navigateTo(path) {
+      router.push(path);
+    },
   },
   components: {
       HeaderNav,
